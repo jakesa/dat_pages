c3aa8a770a0bdfb52eb04b619fcca65775bc456f
Jake Sarate <jake.sarate@dat.com>
2017-01-19 12:08:24 -0800

    added support for predefined selectors on section objects

--- lib/dat_pages/web_driver/section.rb
+++ lib/dat_pages/web_driver/section.rb
@@ -4,7 +4,17 @@ require_relative 'elements/element_container'
 class DATPages::WebDriver::PageObjects::Section < DATPages::WebDriver::PageObjects::Element
   extend DATPages::WebDriver::PageObjects::ElementContainer
 
+  def self.locator
+    @locator
+  end
+
+  def self.locator=(value)
+    @locator=value
+  end
+
+
   def initialize(locator=nil, parent = nil, find_by=:css)
-    super
+    locator = self.locator ? self.locator : locator
+    super(locator, parent, find_by)
   end
 end
